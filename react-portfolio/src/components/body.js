1d4b6b6d15eb7c20bae15ecc4e5469814f65493b
Badara Dia <pushsoccer79@gmail.com>
2022-07-12 19:06:44 -0400

    final commit

---
+++ react-portfolio/src/components/body.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Body() {
+   const BodyCSS = {
+       color: "green",
+       border: "1px solid black",
+       marginBottom: "15px"
+} 
+    return(
+        <div style={bodyCSS}>This is the body page</div>
+    )
+    
+}
