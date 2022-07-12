4c793a06c3e8a1f655583702246333ec84756644
Badara Dia <pushsoccer79@gmail.com>
2022-07-12 19:09:32 -0400

    updates

--- react-portfolio/src/components/body.js
+++ react-portfolio/src/components/body.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Body() {
-   const BodyCSS = {
+   const bodyCSS = {
        color: "green",
        border: "1px solid black",
        marginBottom: "15px"
